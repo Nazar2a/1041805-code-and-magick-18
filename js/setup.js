1a1dba3b7dd9fdad248ac51eed82b6ec0cefd5a7
Nazar2a <50366818+Nazar2a@users.noreply.github.com>
2019-10-10 09:23:07 +0300

    created an array fill function

--- js/setup.js
+++ js/setup.js
@@ -1,12 +1,45 @@
 'use strict';
 
-var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var WIZARD_NAMES = [
+  'Иван',
+  'Хуан Себастьян',
+  'Мария',
+  'Кристоф',
+  'Виктор',
+  'Юлия',
+  'Люпита',
+  'Вашингтон'
+];
 
-var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var WIZARD_SURNAMES = [
+  'да Марья',
+  'Верон',
+  'Мирабелла',
+  'Вальц',
+  'Онопко',
+  'Топольницкая',
+  'Нионго',
+  'Ирвинг'
+];
 
-var WIZARD_COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var WIZARD_COAT_COLORS = [
+  'rgb(101, 137, 164)',
+  'rgb(241, 43, 107)',
+  'rgb(146, 100, 161)',
+  'rgb(56, 159, 117)',
+  'rgb(215, 210, 55)',
+  'rgb(0, 0, 0)'
+];
 
-var WIZARD_EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var WIZARD_EYES_COLORS = [
+  'black',
+  'red',
+  'blue',
+  'yellow',
+  'green'
+];
+
+var wizards = [];
 
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
@@ -27,37 +60,26 @@ var shuffleArr = function (arr) {
   return arr;
 };
 
-shuffleArr(WIZARD_NAMES);
-shuffleArr(WIZARD_SURNAMES);
-shuffleArr(WIZARD_COAT_COLORS);
-shuffleArr(WIZARD_EYES_COLORS);
+var playerStyleCreation = function (N) {
+  shuffleArr(WIZARD_NAMES);
+  shuffleArr(WIZARD_SURNAMES);
+  shuffleArr(WIZARD_COAT_COLORS);
+  shuffleArr(WIZARD_EYES_COLORS);
 
-var wizards = [
-  {
-    name: WIZARD_NAMES[0] + ' ' + WIZARD_SURNAMES[0],
-    coatColor: WIZARD_COAT_COLORS[0],
-    eyesColor: WIZARD_EYES_COLORS[0]
-  },
-  {
-    name: WIZARD_NAMES[1] + ' ' + WIZARD_SURNAMES[1],
-    coatColor: WIZARD_COAT_COLORS[1],
-    eyesColor: WIZARD_EYES_COLORS[1]
-  },
-  {
-    name: WIZARD_NAMES[2] + ' ' + WIZARD_SURNAMES[2],
-    coatColor: WIZARD_COAT_COLORS[2],
-    eyesColor: WIZARD_EYES_COLORS[2]
-  },
-  {
-    name: WIZARD_NAMES[3] + ' ' + WIZARD_SURNAMES[3],
-    coatColor: WIZARD_COAT_COLORS[3],
-    eyesColor: WIZARD_EYES_COLORS[3]
+  for (var i = 0; i <= N; i++) {
+  wizards.unshift({
+    name: WIZARD_NAMES[i] + ' ' + WIZARD_SURNAMES[i],
+    coatColor: WIZARD_COAT_COLORS[i],
+    eyesColor: WIZARD_EYES_COLORS[i]
+  });
   }
-];
+  return wizards;
+};
+
+playerStyleCreation(4);
 
 var renderWizard = function (wizard) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
-
   wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
   wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
   wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
